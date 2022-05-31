3236692fe9ce77269137036f844f3f6d45590f21
AdamMa <adam_ma520@outlook.com>
2022-05-31 15:12:03 +0800

    Adam_BigData_Review:    Flink_Realtime_Project_Gmall_Review : DWD: 03-交易域下单事务事实表

---
+++ flink_project_demo/src/main/java/com/atguigu/gmall/realtime/app/dwd/db/DwdTradeOrderDetail.java
@@ -0,0 +1,119 @@
+package com.atguigu.gmall.realtime.app.dwd.db;
+
+import com.atguigu.gmall.realtime.util.MyKafkaUtil;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+/**
+ * @author Adam-Ma
+ * @date 2022/5/31 15:08
+ * @Project BigData_Review_Adam
+ * @email Adam_Ma520@outlook.com
+ * @phone 18852895353
+ * DESC : 交易域下单事务事实表
+ *
+ * 执行流程：
+ *      从 订单预处理表中获取数据， 过滤出 type = insert 的数据，就是 下单数据
+ */
+public class DwdTradeOrderDetail {
+    public static void main(String[] args) {
+        // TODO 1. 环境准备
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        // TODO 2. 启用状态后端(略)
+
+        // TODO 3. 读取 Kafka dwd_trade_order_pre_process 主题数据
+        tableEnv.executeSql("" +
+                "create table dwd_trade_order_pre_process(\n" +
+                "id string,\n" +
+                "order_id string,\n" +
+                "user_id string,\n" +
+                "order_status string,\n" +
+                "sku_id string,\n" +
+                "sku_name string,\n" +
+                "province_id string,\n" +
+                "activity_id string,\n" +
+                "activity_rule_id string,\n" +
+                "coupon_id string,\n" +
+                "date_id string,\n" +
+                "create_time string,\n" +
+                "operate_date_id string,\n" +
+                "operate_time string,\n" +
+                "source_id string,\n" +
+                "source_type string,\n" +
+                "source_type_name string,\n" +
+                "sku_num string,\n" +
+                "split_original_amount string,\n" +
+                "split_activity_amount string,\n" +
+                "split_coupon_amount string,\n" +
+                "split_total_amount string,\n" +
+                "`type` string,\n" +
+                "`old` map<string,string>,\n" +
+                "od_ts string,\n" +
+                "oi_ts string,\n" +
+                "row_op_ts timestamp_ltz(3)\n" +
+                ")" + MyKafkaUtil.getKafkaDDL(
+                "dwd_trade_order_pre_process", "dwd_trade_order_detail"));
+
+        // TODO 4. 过滤下单数据
+        Table filteredTable = tableEnv.sqlQuery("" +
+                "select " +
+                "id,\n" +
+                "order_id,\n" +
+                "user_id,\n" +
+                "sku_id,\n" +
+                "sku_name,\n" +
+                "province_id,\n" +
+                "activity_id,\n" +
+                "activity_rule_id,\n" +
+                "coupon_id,\n" +
+                "date_id,\n" +
+                "create_time,\n" +
+                "source_id,\n" +
+                "source_type source_type_code,\n" +
+                "source_type_name,\n" +
+                "sku_num,\n" +
+                "split_original_amount,\n" +
+                "split_activity_amount,\n" +
+                "split_coupon_amount,\n" +
+                "split_total_amount,\n" +
+                "od_ts ts,\n" +
+                "row_op_ts\n" +
+                "from dwd_trade_order_pre_process " +
+                "where `type`='insert'");
+        tableEnv.createTemporaryView("filtered_table", filteredTable);
+
+        // TODO 5. 创建 Kafka 下单明细表
+        tableEnv.executeSql("" +
+                "create table dwd_trade_order_detail(\n" +
+                "id string,\n" +
+                "order_id string,\n" +
+                "user_id string,\n" +
+                "sku_id string,\n" +
+                "sku_name string,\n" +
+                "province_id string,\n" +
+                "activity_id string,\n" +
+                "activity_rule_id string,\n" +
+                "coupon_id string,\n" +
+                "date_id string,\n" +
+                "create_time string,\n" +
+                "source_id string,\n" +
+                "source_type_code string,\n" +
+                "source_type_name string,\n" +
+                "sku_num string,\n" +
+                "split_original_amount string,\n" +
+                "split_activity_amount string,\n" +
+                "split_coupon_amount string,\n" +
+                "split_total_amount string,\n" +
+                "ts string,\n" +
+                "row_op_ts timestamp_ltz(3),\n" +
+                "primary key(id) not enforced\n" +
+                ")" + MyKafkaUtil.getUpsertKafkaDDL("dwd_trade_order_detail"));
+
+        // TODO 6. 将数据写出到 Kafka
+        tableEnv.executeSql("insert into dwd_trade_order_detail select * from filtered_table");
+    }
+}
