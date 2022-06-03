99f43ba3b2ce8a0bc3bf674d4cc85c3a07b65225
AdamMa <adam_ma520@outlook.com>
2022-06-03 20:46:35 +0800

    Adam_BigData_Review:    Flink_Realtime_Project_Gmall_Review : DWS：01-流量域页面关键词各窗口聚合

--- flink_project_demo/src/main/java/com/atguigu/gmall/realtime/app/dws/DwsTrafficSourceKeywordPageViewWindow.java
+++ flink_project_demo/src/main/java/com/atguigu/gmall/realtime/app/dws/DwsTrafficSourceKeywordPageViewWindow.java
@@ -24,9 +24,36 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  * DESC : 流量域来源关键词粒度页面浏览各窗口轻度聚合
  *
  * 需要开启的组件：
- *
+ *      flume , zk, Kafka , [HDFS],DwdTrafficBaseLogSplit, DwsTrafficSourceKeywordPageViewWindow
  * 执行流程：
- *      1）
+ *      1）flume 采集神策大数据提供的前端用户埋点日志数据，将日志数据存储到 Kafka 中的 topic_log主题中, 作为 ODS层的日志数据
+ *      2）Flink 程序读取 ODS层的 topic_log 中的数据，将topic_log 进行分流，将分流的结果保存到 Kafka 各个流量域主题中，作为 流量域事务事实表
+ *          启动日志： dwd_traffic_start_log
+ *          页面日志： dwd_traffic_page_log
+ *          曝光日志： dwd_traffic_display_log
+ *          动作日志： dwd_traffic_action_log
+ *          错误日志： dwd_traffic_error_log
+ *      3) 由于关键词只是在 页面日志中存在，当前读取Kafka 数据采用FlinkSQL 方式，
+ *         所以创建动态表，读取 dwd_traffic_page_log 数据
+ *      4） 由于关键词所在页面的 上一页为 search，当前的项目类型为 keyword，项目不为空，所以对当前页面进行判断
+ *              last_page_id = search
+ *              item_type = keyword
+ *              item != null
+        5) 根据以上条件进行过滤，过滤出包含关键词的页面
+        6）由于需要将搜索内容进行分词操作，当前使用 ik 分词器，编写 ik 分词工具类
+        7）由于使用 FlinkSQL，要在 sql 中使用分词功能，直接使用工具类调用方法肯定是不行的。
+            所以需要自定义一个函数，用于将搜索内容拆分为多个关键词，类似Hive 中炸裂函数
+           所以需要自定义 UDTF (User Definied Table Function)
+        8）由于拆分后的每个关键词需要与原来的数据构成一行，所以需要类似 Hive 中的 侧写功能，
+            在Flink 官网中的 sql - join 中提供了侧写的使用方式
+        9）炸裂，侧写好数据后，需要将数据按照关键词进行分组
+        10）将分好组的数据 进行开窗（官网中也提供了 分组后使用窗口函数的方式），聚合统计
+        11）由于需要进行开窗，所以需要提前指定水位线，在FlinkSQL中，指定水位线是需要在创建表的时候指定水位线的。
+        12）将统计好的数据，写入到 ClickHouse进行存储
+             （1） FlinkSQL没有提供与ClickHouse 的连接器，所以需要将 FlinkSQL 中的 Table 转换成 流对象
+                  FlinkSQL 中的数据要转换成流，那么在流中以什么类型保存呢，所以就需要提供一个类，用来指明sql中的数据转换成流后以什么类型存在
+            （2）转换成流后，将流中的数据写入到ClickHouse 中
+                    提供ClickHouse 工具类
  */
 public class DwsTrafficSourceKeywordPageViewWindow {
     public static void main(String[] args) throws Exception {
